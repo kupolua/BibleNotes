cee6295f9de5dcd6c0fdbb63727816208f012ad1
dankul <daniilkulakovskiu@gmail.com>
2019-01-10 11:22:10 +0200

    update project

--- src/containers/Menu/index.js
+++ src/containers/Menu/index.js
@@ -6,11 +6,13 @@ const options = [
     { key: 3, text: 'О проекте', value: 3 },
 ];
 
-export default class NotesMenu extends React.Component{
+export default class NotesMenu extends React.Component {
     render () {
         return (
             <Menu>
-                <Dropdown item simple text='Меню' direction='left' options={options} />
+                <Dropdown item simple text='Меню' direction='left' options={options} button={2}/>
+                <Menu.Item>Note</Menu.Item>
+                <Menu.Item>Read</Menu.Item>
             </Menu>
         )
     }
